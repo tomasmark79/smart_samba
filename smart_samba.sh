c4b23f4cf403e8b259ece5df255e809b0718d6f7
tomasmark79 <tomas@digitalspace.name>
2024-11-07 19:07:40 +0100

    shellcheck

--- smart_samba.sh
+++ smart_samba.sh
@@ -8,14 +8,14 @@ log() {
     fi
 }
 
-umount_samba() {
-    sudo umount -f -l "$MOUNT_POINT" >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        log "Successfully Unmounted from $MOUNT_POINT."
-    else
-        log "Already Unmounted from $MOUNT_POINT."
-    fi
-}
+# umount_samba() {
+#     sudo umount -f -l "$MOUNT_POINT" >/dev/null 2>&1
+#     if [ $? -eq 0 ]; then
+#         log "Successfully Unmounted from $MOUNT_POINT."
+#     else
+#         log "Already Unmounted from $MOUNT_POINT."
+#     fi
+# }
 
 smart_samba_mount() {
     echo "smart_samba_mount() {" 
@@ -59,7 +59,8 @@ smart_samba_mount() {
             log "Destination IP $remote_ip for $mount_point is $ping_result. (preventive umount)"
             
             # Magic is umount in screen session to smooth the process
-            screen -dmS umount_samba bash -c "sudo umount -f -l $mount_point"
+            # screen -dmS umount_samba bash -c "sudo umount -f -l $mount_point"
+            screen -dmS bash -c "sudo umount -f -l $mount_point"
             
             # Clean up
             rm -f "$lock_file"
@@ -76,9 +77,10 @@ smart_samba_mount() {
         log "Samba share $remote_samba_share already connected to $mount_point."
     else
         Connection to Samba share if ping and port check passed
-        if [ "$ping_result" == "accessible" ] && ([ "$port_result" == "$port_status" ] || [ "$port_check" != "yes" ]); then
-            sudo mount -t cifs -o noatime,guest,_netdev,vers=3.0,noauto,credentials=$credential_file,uid=1000,gid=1000,iocharset=utf8,sec=ntlmssp,nofail,soft,noperm "$remote_samba_share" "$mount_point"
-            if [ $? -eq 0 ]; then
+        if [ "$ping_result" == "accessible" ] && { [ "$port_result" == "$port_status" ] || [ "$port_check" != "yes" ]; }; then
+            #sudo mount -t cifs -o noatime,guest,_netdev,vers=3.0,noauto,credentials="$credential_file",uid=1000,gid=1000,iocharset=utf8,sec=ntlmssp,nofail,soft,noperm "$remote_samba_share" "$mount_point"
+            if sudo mount -t cifs -o noatime,guest,_netdev,vers=3.0,noauto,credentials="$credential_file",uid=1000,gid=1000,iocharset=utf8,sec=ntlmssp,nofail,soft,noperm "$remote_samba_share" "$mount_point"; then
+            #if [ $? -eq 0 ]; then
                 log "Samba share $remote_samba_share was successfully connected to $mount_point."
             else
                 log "Samba share $remote_samba_share was unsuccessfully connected to $mount_point. (Failed)"
@@ -87,7 +89,8 @@ smart_samba_mount() {
             log "Samba share $remote_samba_share was not connected to $mount_point. (filtered, preventive umount)"
             
             # Magic is umount in screen session to smooth the process
-            screen -dmS umount_samba bash -c "sudo umount -f -l $mount_point"
+            # screen -dmS umount_samba bash -c "sudo umount -f -l $mount_point" #puvodni udajne spatne
+            screen -dmS bash -c "sudo umount -f -l $mount_point"
         fi
     fi
 
@@ -98,10 +101,10 @@ smart_samba_mount() {
 # Params:         CREDENTIAL_FILE        SAMBA_SHARE             TARGET_IP       MOUNT_POINT PING_CHECK PORT_CHECK PORT_NUMBER PORT_STATUS
 
 # Nas with opened port 7922
-smart_samba_mount "/root/credential2nas" "//192.168.79.11/homes" "192.168.79.11" "/mnt/nas/" yes yes 7922 opened
-smart_samba_mount "/root/credential2nas" "//192.168.79.11/music" "192.168.79.11" "/mnt/nas/music" yes yes 7922 opened
-smart_samba_mount "/root/credential2nas" "//192.168.79.11/video" "192.168.79.11" "/mnt/nas/video" yes yes 7922 opened
-smart_samba_mount "/root/credential2nas" "//192.168.79.11/NetBackup" "192.168.79.11" "/mnt/nas/NetBackup" yes yes 7922 opened
+#smart_samba_mount "/root/credential2nas" "//192.168.79.11/homes" "192.168.79.11" "/mnt/nas/" yes yes 7922 opened
+#smart_samba_mount "/root/credential2nas" "//192.168.79.11/music" "192.168.79.11" "/mnt/nas/music" yes yes 7922 opened
+#smart_samba_mount "/root/credential2nas" "//192.168.79.11/video" "192.168.79.11" "/mnt/nas/video" yes yes 7922 opened
+#smart_samba_mount "/root/credential2nas" "//192.168.79.11/NetBackup" "192.168.79.11" "/mnt/nas/NetBackup" yes yes 7922 opened
 
 # MS Windows with closed port 7922
 smart_samba_mount "/root/credential2pc" "//192.168.79.111/c" "192.168.79.111" "/mnt/pcwin/c" yes yes 7922 closed
@@ -112,3 +115,5 @@ smart_samba_mount "/root/credential2pc" "//192.168.79.111/f" "192.168.79.111" "/
 # Libvirts Windows
 smart_samba_mount "/root/credential2winku" "//192.168.78.100/c" "192.168.78.100" "/mnt/winku/c" yes yes 7922 closed
 smart_samba_mount "/root/credential2winku" "//192.168.78.144/c" "192.168.78.144" "/mnt/windev/c" yes yes 7922 closed
+
+
